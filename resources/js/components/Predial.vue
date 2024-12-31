fe3779dbf753c1913aa51a277da0c7ff8ae93119
Jose Aispuro <joseaispuro2481@gmail.com>
2024-12-31 19:42:35 +0000

    dash

--- resources/js/components/Predial.vue
+++ resources/js/components/Predial.vue
@@ -2,27 +2,50 @@
   <h1>Estadísticas de Impuesto Predial Urbano</h1>
   <div class="containter">
       <div class="row">
-        <div class="col-md-3">
-          <h3>Período</h3>
-          <input type="date" class="form-control"  v-model="desde">
-          <input type="date" class="form-control" v-model="hasta">
-          <button @click="actualizar" class="btn btn-primary">
-            Actualizar
-          </button>
-        </div>
-        <div class="col-md-6">
 
-            <Bar
-              id="workhr-chart"
-              :options="chartOptions"
-              :data="chartData"
-              :style="chartStyles"
-            >
-            </Bar>
+        <div class="col-md-9">
+
+          <div class="row">
+
+            <div class="col-md-6 sombra">
+              <h5>Período</h5>
+              <!--
+              <input type="date" class="form-control"  v-model="desde">
+              <input type="date" class="form-control" v-model="hasta">
+              <div class="d-grid gap-2">
+                <button class="btn btn-primary" @click="actualizar" type="button">Actualizar</button>
+              </div>-->
+            </div>
+
+            <div class="col-md-6 sombra">
+                <h5>Ingreso Digital</h5>
+                <p class="numero">$59.01 M</p>
+                <p class="leyenda">1,824 pagos</p>
+            </div>
+           
+          </div>
         </div>
-        <div class="col-md-6">
-          {{info}}
+
+        <div class="col-md-3">
+
+          <div class="row">
+             <h5>Distribución Bancos</h5>
+           
+             <Bar
+                id="workhr-chart"
+                :options="chartOptions"
+                :data="chartData"
+                :style="chartStyles"
+              >
+              </Bar>
+          </div>
+           {{info}}
         </div>
+
+        
+
+        
+
     </div>
   </div>
 </template>
@@ -117,5 +140,26 @@ h3{
 }
 h1{
   margin-bottom: 40px;
+}
+h5{
+  font-size: 1em;
+  color: #797979;
+  padding-top: 5px;
+}
+p.numero{
+  font-size: 3.2em;
+  text-align: center;
+  font-weight: bold;
+}
+p.leyenda{
+  text-align: center;
+  font-size: 1.2em;
+  font-weight: bolder;
+  color: gray;
+}
+.sombra{
+-webkit-box-shadow: 6px 3px 14px -3px rgba(0,0,0,0.75);
+-moz-box-shadow: 6px 3px 14px -3px rgba(0,0,0,0.75);
+box-shadow: 6px 3px 14px -3px rgba(0,0,0,0.75);
 }
 </style>
